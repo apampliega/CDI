095d1b02b25427f241f0b6b91bba9e3cefa2addb
Alberto702 <sshooter42@gmail.com>
2020-04-24 19:32:22 +0200

    persistencia boton Encender/apagar.html y index.js

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
-/*
+// guardar datos nevera
 $(document).ready(function(){
-$('.simple-radio li').click(function() {
-
-
-  $('.simple-radio li').removeAttr('class');
-  $(this).addClass('checked');
+$('.frigoOn ').click(function() {
+	$('.frigoOn ').removeClass('selected');
+	$('.frigoOff ').removeClass('selected');
+	$(this).addClass('selected');
+	//localstorage
+	localStorage.setItem('estadoFrigorifico', 'on');
 });
-});*/
+
+$('.frigoOff ').click(function() {
+	$('.frigoOn ').removeClass('selected');
+	$('.frigoOff ').removeClass('selected');
+	$(this).addClass('selected');
+  //localstorage
+	localStorage.setItem('estadoFrigorifico', 'off');
+});
+
+});
+
+//lee la variable y aplica la clase
+ $(document).ready(function(){   
+
+    if(localStorage.getItem('estadoFrigorifico') == 'on') {
+        $(".frigoOn").addClass("selected");
+        $(".frigoOff").removeClass("selected");
+    } else{
+    	$(".frigoOff").addClass("selected");
+    	$(".frigoOn").removeClass("selected");
+    }
+ }); 
 
 
+
+
+ //guardar datos congelador
 $(document).ready(function(){
-$('.estado_frigorifico button').click(function() {
-
-
- //$('.estado_frigorifico button').removeAttr('class');
-  $('.estado_frigorifico button').removeClass('selected');
-  $(this).addClass('selected');
+$('.congOn ').click(function() {
+	$('.congOn ').removeClass('selected');
+	$('.congOff ').removeClass('selected');
+	$(this).addClass('selected');
+	//localstorage
+	localStorage.setItem('estadoCongelador', 'on');
 });
+
+$('.congOff ').click(function() {
+	$('.congOn ').removeClass('selected');
+	$('.congOff ').removeClass('selected');
+	$(this).addClass('selected');
+  //localstorage
+	localStorage.setItem('estadoCongelador', 'off');
 });
 
-
-$(document).ready(function(){
-$('.estado_congelador button').click(function() {
-
-
- //$('.estado_frigorifico button').removeAttr('class');
-  $('.estado_congelador button').removeClass('selected');
-  $(this).addClass('selected');
 });
-});
+
+//lee la variable y aplica la clase
+ $(document).ready(function(){   
+
+    if(localStorage.getItem('estadoCongelador') == 'on') {
+        $(".congOn").addClass("selected");
+        $(".congOff").removeClass("selected");
+    } else{
+    	$(".congOff").addClass("selected");
+    	$(".congOn").removeClass("selected");
+    }
+ }); 
+
+
+
